e215db4f46d36b2dea1db428be7822a2f842dd1b
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-04-16 20:43:19 +0200

    refactor if_scrambled_palindrome.py

--- if_scrambled_palindrome.py
+++ if_scrambled_palindrome.py
@@ -7,18 +7,18 @@ def if_palindrome(input_str):
     len_str_unique = len(np.unique(sorted_str))
     if len_str == 1:
         return True
-    elif (len_str_unique == 1):
+    elif len_str_unique == 1:
         return True
     elif (len_str - len_str_unique) == 0:
         return False
-    elif ((len_str - len_str_unique)%2 == 0 and len_str%2 == 0):
+    elif (len_str - len_str_unique) % 2 == 0 and len_str % 2 == 0:
         return False
-    elif ((len_str - len_str_unique)%2 == 0 and len_str%2 == 1):
+    elif (len_str - len_str_unique) % 2 == 0 and len_str % 2 == 1:
         if len_str_unique % 2 == 0:
             return False
         else:
             return True
-    elif ((len_str - len_str_unique) % 2 == 1):
+    elif (len_str - len_str_unique) % 2 == 1:
         if len_str_unique % 2 == 0:
             return True
         else:
@@ -28,7 +28,7 @@ def if_palindrome(input_str):
 
 
 if __name__ == '__main__':
-    text=''
-    while(text!='e'):
+    text = ''
+    while text != 'e':
         text = input('Wpisz slowo: ')
         print(if_palindrome(text))
